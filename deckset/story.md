64fef1186c4b16cfde0207acc696a05ee24377c5
Pierangelo Cecchetto <pierangeloc@gmail.com>
2023-11-15 00:58:32 +0100

    Charging hub defined and runnable

--- deckset/story.md
+++ deckset/story.md
@@ -9,3 +9,10 @@ Ways to achieve observability:
 - We need to pass context between services
 
 
+Steps to build an instrumented Grpc application:
+
+1. Override ZIO default loggers
+2. make a class that extends the ZIOXXXAPI
+3. define a method that returns a Server, given the ZIOXXXAPI, by adding the service to the service list
+3. Build a layer that provides the ZIOXXXAPI `GrpcServerTracingInterpreter.serviceWithTracing` method, that will add and use all the tracing dependencies
+4.
--- modules/example/charging-hub/src/main/scala/charginghub/ChargingHub.scala
+++ modules/example/charging-hub/src/main/scala/charginghub/ChargingHub.scala
@@ -1,10 +1,43 @@
 package charginghub
 
-import io.tuliplogic.ziotoolbox.tracing.commons.Bootstrap
-import zio.{Scope, ZIO, ZIOAppArgs, ZIOAppDefault, ZLayer}
+import io.grpc.{ServerBuilder, StatusException}
+import io.tuliplogic.ziotoolbox.tracing.commons.{Bootstrap, OTELTracer}
+import io.tuliplogic.ziotoolbox.tracing.example.proto.charging_hub_api.{SessionDetails, StartSessionResponse, ZioChargingHubApi}
+import io.tuliplogic.ziotoolbox.tracing.grpc.server.GrpcServerTracingInterpreter
+import scalapb.zio_grpc.{RequestContext, Server, ServerLayer, ServiceList}
+import zio.{IO, Scope, ZIO, ZIOAppArgs, ZIOAppDefault, ZLayer}
 
 object ChargingHub extends ZIOAppDefault {
   override val bootstrap: ZLayer[ZIOAppArgs, Any, Environment] =
     Bootstrap.defaultBootstrap
-  override def run: ZIO[Any with ZIOAppArgs with Scope, Any, Any] = ???
+
+  class ApiImpl extends ZioChargingHubApi.ChargingHubApi {
+    override def startSession(request: SessionDetails): IO[StatusException, StartSessionResponse] =
+    zio.Random.nextBoolean.map(StartSessionResponse(_))
+  }
+
+  object ApiImpl {
+    val layer = GrpcServerTracingInterpreter.serviceWithTracing[Any, ZioChargingHubApi.ChargingHubApi](_ =>
+      new ApiImpl
+    )
+  }
+
+  private def serverLive(
+                          port: Int
+                        ): ZLayer[ZioChargingHubApi.GChargingHubApi[RequestContext, StatusException], Throwable, Server] = {
+    val serviceList = ServiceList.addFromEnvironment[ZioChargingHubApi.GChargingHubApi[RequestContext, StatusException]]
+    ServerLayer.fromServiceList(ServerBuilder.forPort(port), serviceList)
+  }
+
+  private def makeLaunchableServer(port: Int) =
+    ZLayer.make[Server](
+      serverLive(port),
+      ChargingHub.ApiImpl.layer,
+      Bootstrap.tracingLayer,
+      OTELTracer.default("charging-hub"),
+    )
+
+
+  override def run: ZIO[Any with ZIOAppArgs with Scope, Any, Any] =
+    makeLaunchableServer(9001).launch
 }
